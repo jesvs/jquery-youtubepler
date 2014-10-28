43d1413462c86d3f2c46a65cea5450af7f4e0901
jesvs <ahinosvemos@gmail.com>
2014-10-28 14:10:40 -0600

    initial commit

---
+++ jquery.youtubepler.coffee
@@ -0,0 +1,43 @@
+(($) ->
+  $.fn.youtubepler = (opts) ->
+    parseYouTubeUrl = (url) ->
+      video_id = url.split('v=')[1]
+      ampPos   = video_id.indexOf '&'
+      if ampPos != -1
+        video_id = video_id.substring 0, ampPos
+      video_id
+
+    player_container = $(opts.player)
+    @player = null
+
+    # get video ids
+    video_ids = []
+    links = this.find 'a'
+    for link in links
+      href = link.href
+      continue unless href.indexOf 'youtube' > -1   # skip if link isn't youtube
+      video_ids.push( parseYouTubeUrl( href ) )     # store youtube id in array
+      
+      # link click listener
+      $(link).on 'click', (event) =>
+        event.preventDefault()
+        event.stopPropagation()
+        links.removeClass 'active'
+        $(this).addClass 'active'
+        index = @player.getPlaylist().indexOf(parseYouTubeUrl(event.target.href))
+        @player.playVideoAt index
+
+    # create player
+    player_container.append $("<script src='https://www.youtube.com/iframe_api'>")
+    window.onYouTubeIframeAPIReady = () =>
+      @player = new YT.Player 'player',
+        height: 390
+        width: 640
+        events:
+          'onReady': onPlayerReady
+    
+    onPlayerReady = (event) =>
+      event.target.cuePlaylist video_ids
+
+    return this
+)(jQuery)
---
+++ jquery.youtubepler.js
@@ -0,0 +1,57 @@
+(function($) {
+  return $.fn.youtubepler = function(opts) {
+    var href, link, links, onPlayerReady, parseYouTubeUrl, player_container, video_ids, _i, _len;
+    parseYouTubeUrl = function(url) {
+      var ampPos, video_id;
+      video_id = url.split('v=')[1];
+      ampPos = video_id.indexOf('&');
+      if (ampPos !== -1) {
+        video_id = video_id.substring(0, ampPos);
+      }
+      return video_id;
+    };
+    player_container = $(opts.player);
+    this.player = null;
+    video_ids = [];
+    links = this.find('a');
+    for (_i = 0, _len = links.length; _i < _len; _i++) {
+      link = links[_i];
+      href = link.href;
+      if (!href.indexOf('youtube' > -1)) {
+        continue;
+      }
+      video_ids.push(parseYouTubeUrl(href));
+      $(link).on('click', (function(_this) {
+        return function(event) {
+          var index;
+          event.preventDefault();
+          event.stopPropagation();
+          links.removeClass('active');
+          $(_this).addClass('active');
+          index = _this.player.getPlaylist().indexOf(parseYouTubeUrl(event.target.href));
+          return _this.player.playVideoAt(index);
+        };
+      })(this));
+    }
+    player_container.append($("<script src='https://www.youtube.com/iframe_api'>"));
+    window.onYouTubeIframeAPIReady = (function(_this) {
+      return function() {
+        return _this.player = new YT.Player('player', {
+          height: 390,
+          width: 640,
+          events: {
+            'onReady': onPlayerReady
+          }
+        });
+      };
+    })(this);
+    onPlayerReady = (function(_this) {
+      return function(event) {
+        return event.target.cuePlaylist(video_ids);
+      };
+    })(this);
+    return this;
+  };
+})(jQuery);
+
+//# sourceMappingURL=jquery.youtubepler.js.map
